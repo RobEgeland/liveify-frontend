15bf8e34dd02dab67f898075d27f9d7d68c3b645
Robert Egeland <robegeland@yahoo.com>
2022-07-08 16:34:06 -0500

    test 2

--- src/components/UpdateConcert.js
+++ src/components/UpdateConcert.js
@@ -37,7 +37,7 @@ const UpdateConcert = ({artists, concerts}) => {
         venue:updateConcert.venue,
         artist_id: updateConcert.artist_id
       })
-      console.o
+      console.log()
     },[])
     
 
